feff3bda0f7c17e92cebf9ae9cbab5045be991ee
zeiosis <101180649+zeiosis@users.noreply.github.com>
2022-06-15 16:39:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,9 @@ summary = st.text_input('Work summary to predict from', 'Two knights set off on 
 classifier = pipeline(model='zdreiosis/ff_analysis_4', return_all_scores=True)
 labels = classifier([summary])
 
+ksorted = [sorted(labels[y], key=lambda x: x['score'], reverse=True) for y in range(len(labels))]
+
 st.write('Predicted labels: ')
-for x in labels[0]:
+for x in ksorted[0]:
   st.write(x['label'] + ':', x['score'])
 
