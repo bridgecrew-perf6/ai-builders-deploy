bd64a0519080bf756ff51897dfc9d7fc11e332e6
zeiosis <101180649+zeiosis@users.noreply.github.com>
2022-06-15 15:59:46 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 import streamlit as st
 from transformers import pipeline
 
-classifier = pipeline(model='zdreiosis/ff_analysis_4', return_all_scores=True)
-labels = classifier(['Two knights go on an adventure.'])
+summary = st.text_input('Work summary to predict from', 'Two knights set off on an adventure.')
+st.write('Work summary: ', title)
 
-st.write(labels)
+#classifier = pipeline(model='zdreiosis/ff_analysis_4', return_all_scores=True)
+#labels = classifier(['Two knights go on an adventure.'])
+
+#st.write(labels)
 
 
