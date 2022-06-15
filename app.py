292792ec1702ff2e61cc7b9876e57e5dbee2a1ee
zeiosis <101180649+zeiosis@users.noreply.github.com>
2022-06-15 15:40:57 +0700

    testing

--- app.py
+++ app.py
@@ -1,25 +1,16 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+import transformers
+from transformers import pipeline
+import torch
+cuda_id = torch.cuda.current_device()
 
 df = pd.DataFrame({
     'first column': [1, 2, 3, 4],
     'second column': [10, 20, 30, 40]
 })
 
-st.write("Here's our first table:")
-st.write(df)
+st.write(cuda_id)
 
-chart_data = pd.DataFrame(
-    np.random.randn(20, 3),
-    columns=['a', 'b', 'c']
-)
-
-st.line_chart(chart_data)
-
-map_data = pd.DataFrame(
-    np.random.randn(1000, 2) / [50, 50] + [13.76514, 100.53829],
-    columns=['lat', 'lon']
-)
-
-st.map(map_data)
+classifier = pipeline(model='zdreiosis/ff_analysis_4', return_all_scores=True, device=cuda_id)
