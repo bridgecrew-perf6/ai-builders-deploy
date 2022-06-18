eb61829d18879456de576ff9eef26da0c17eabfc
zeiosis <101180649+zeiosis@users.noreply.github.com>
2022-06-18 23:09:36 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ import streamlit as st
 from transformers import pipeline
 
 summary = st.text_area('Work summary to predict from', value='Two knights set off on an adventure.')
-
-classifier = pipeline(model='zdreiosis/ff_analysis_4', return_all_scores=True)
+predmodel = st.selectbox(['zdreiosis/ff_analysis_5', 'zdreiosis/ff_analysis_4'])
+classifier = pipeline(model=predmodel, return_all_scores=True)
 labels = classifier([summary])
 
 ksorted = [sorted(labels[y], key=lambda x: x['score'], reverse=True) for y in range(len(labels))]
