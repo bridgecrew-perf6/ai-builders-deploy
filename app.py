f3fc98eeefa3ffc0c9928c5604d7b5d15d41f2d2
zeiosis <101180649+zeiosis@users.noreply.github.com>
2022-06-19 23:37:46 +0700

    Update app.py

--- app.py
+++ app.py
@@ -23,6 +23,6 @@ st.write('Predicted labels: ')
 for x in ksorted[0]:
   st.write(x['label'] + ':', x['score'])
   
-st.write('--')
+st.write('---')
 st.write('check out the [blog post!](https://medium.com/@syntrp2/nlp-for-genre-predictions-on-ffnet-an-antithesis-to-utilitarianism-4380524ca1fc)')
 
