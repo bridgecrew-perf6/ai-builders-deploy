cf21b4f67c80edff9b96035fd015a9580b000995
zeiosis <101180649+zeiosis@users.noreply.github.com>
2022-06-19 23:31:53 +0700

    Update app.py

--- app.py
+++ app.py
@@ -10,11 +10,13 @@ elif predmodel == 'zdreiosis/ff_analysis_5':
   revisionlist = ('1454370', '4a08027', '3f1f66b', 'b5f60f2', '1c7d5f4')
 
 revision = st.selectbox('model version', revisionlist)
-  
+st.write('(Recommended setup: zdr5/1454370)')
+
 classifier = pipeline(model=predmodel, revision=revision, return_all_scores=True)
 labels = classifier([summary])
 
 ksorted = [sorted(labels[y], key=lambda x: x['score'], reverse=True) for y in range(len(labels))]
+
 
 st.write('Predicted labels: ')
 for x in ksorted[0]:
