c6deea68ef19c278abd2eb27676323e6195d30d7
zeiosis <101180649+zeiosis@users.noreply.github.com>
2022-06-19 23:23:39 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,15 @@ from transformers import pipeline
 
 summary = st.text_area('Work summary to predict from', value='Two knights set off on an adventure.')
 predmodel = st.selectbox('model', ('zdreiosis/ff_analysis_5', 'zdreiosis/ff_analysis_4'))
-classifier = pipeline(model=predmodel, return_all_scores=True)
+
+if predmodel == 'zdreiosis/ff_analysis_4':
+  revisionlist = ('998f684', '4c4e9b6', 'bae2482')
+elif predmodel == 'zdreiosis/ff_analysis_5':
+  revisionlist = ('1454370', '4a08027', '3f1f66b', 'b5f60f2', '1c7d5f4')
+
+revision = st.selectbox('model version', revisionlist)
+  
+classifier = pipeline(model=predmodel, revision=revision, return_all_scores=True)
 labels = classifier([summary])
 
 ksorted = [sorted(labels[y], key=lambda x: x['score'], reverse=True) for y in range(len(labels))]
